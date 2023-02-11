27c5bb56fdb7a8f70978ed6b674d771b0c7dd0bb
Artemy Vasiukov <vasiukoff@yandex.ru>
2023-02-11 03:30:51 +0300

    Initial release

--- Setup.bat
+++ Setup.bat
@@ -5,9 +5,6 @@ echo uui - USB unnatended installator for Enterprise Linux
 echo (C) Artemy Vasiukov, avv@cranix.org  2012-2023
 echo Self updating...
 
-rem set debug="yep"
-
-
 if not exist uui git\cmd\git.exe clone https://github.com/vasiukoff/uui.git 
 
 cd uui
@@ -40,20 +37,10 @@ goto next
 :fuckoff
 echo No way to install on drive C:!
 pause
-exit
 
 :next
-if not defined debug (
 
-	echo Installig SYSLINUX..
-	cd syslinux
-	syslinux -m -a  -d /syslinux %drive%
-	pause
-	exit
-
-) else (
-
-	pause
-
-)
-
+echo Installig SYSLINUX..
+cd syslinux
+syslinux -m -a  -d /syslinux %drive%
+pause
